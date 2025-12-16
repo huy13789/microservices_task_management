8739e68b8dcd5b111a836ebcaa5ee2df0851d6c9
huy13789 <70091982+huy13789@users.noreply.github.com>
2025-12-16 23:20:05 +0700

    fix gateway

--- api-gateway/src/main/java/org/example/apigateway/config/SecurityConfig.java
+++ api-gateway/src/main/java/org/example/apigateway/config/SecurityConfig.java
@@ -2,53 +2,30 @@ package org.example.apigateway.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.convert.converter.Converter;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpMethod; // Import thêm cái này
 import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.oauth2.jwt.Jwt;
-import org.springframework.security.oauth2.server.resource.authentication.*;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.server.SecurityWebFilterChain;
-import reactor.core.publisher.Mono;
 
-import java.util.Collection;
-import java.util.List;
-import java.util.stream.Collectors;
-
-
-
-@EnableWebFluxSecurity
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 
     @Bean
     public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
         http
-                .csrf(csrf -> csrf.disable())
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
                 .authorizeExchange(exchanges -> exchanges
-                        .pathMatchers("/api/v1/users/signup", "/api/v1/users/login", "/actuator/**").permitAll()
+                        .pathMatchers(HttpMethod.OPTIONS).permitAll()
+
+                        .pathMatchers("/actuator/**").permitAll()
+
+                        .pathMatchers("/api/v1/users/login", "/api/v1/users/register").permitAll()
+
                         .anyExchange().authenticated()
                 )
-                .oauth2ResourceServer(oauth2 -> oauth2
-                        .jwt(Customizer.withDefaults())
-                        .authenticationEntryPoint((exchange, ex) -> {
-                            exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
-                            exchange.getResponse().getHeaders().add("Content-Type", "application/json; charset=UTF-8");
-                            String body = "{\"error\": \"Bạn chưa đăng nhập hoặc Token hết hạn (Custom Message)\"}";
-                            return exchange.getResponse().writeWith(Mono.just(exchange.getResponse().bufferFactory().wrap(body.getBytes())));
-                        })
-                        .accessDeniedHandler((exchange, denied) -> {
-                            exchange.getResponse().setStatusCode(HttpStatus.FORBIDDEN);
-                            exchange.getResponse().getHeaders().add("Content-Type", "application/json; charset=UTF-8");
-                            String body = "{\"error\": \"Bạn không có quyền truy cập (Access Denied)\"}";
-                            return exchange.getResponse().writeWith(Mono.just(exchange.getResponse().bufferFactory().wrap(body.getBytes())));
-                        })
-                );
+                .oauth2ResourceServer(oauth2 -> oauth2.jwt(Customizer.withDefaults()));
 
         return http.build();
     }
